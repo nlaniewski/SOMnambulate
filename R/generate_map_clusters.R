54ef133d1da01337946ba96ebbe515eeef648765
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2022-12-12 13:15:07 -0500

    more fixes for generate_map_clusters

--- R/generate_map_clusters.R
+++ R/generate_map_clusters.R
@@ -1,44 +1,50 @@
-generate_map_clusters <- function(dat,dims,scale.dims=T,subsample.val=2E5){
-  fsom <- generate_fsom(dat=dat,dims=dims)
-  mc <- generate_clusters()
-  map <- generate_map_nodes(dat=dat)
-  mc[map]
+generate_map_clusters <- function(dat,dims,scale.dims=T,subsample.val=2E5,k=20){
+  codes <- generate_fsom(dat=dat,dims=dims,scale.dims =scale.dims,subsample.val=subsample.val,return.codes = T)
+  mc <- generate_clusters(codes=codes,k=k)
+  map <- generate_map_nodes(codes = codes,dat=dat,scale.dims = scale.dims)
+  return(mc[map])
 }
 
-generate_fsom <- function(dat,dims,scale.dims=T,subsample.val=2E5){
+generate_fsom <- function(dat,dims,scale.dims=T,subsample.val=2E5,return.codes=F){
   dims<-dims[which(dims %in% names(dat))]
   ##
   if(!is.null(subsample.val)&nrow(dat)>subsample.val){
     set.seed(1337)
     if(scale.dims){
-      FlowSOM::SOM(as.matrix(dat[sample(.N,subsample.val), lapply(.SD, function(x) (x - mean(x))/sd(x)),.SDcols=dims]),
-                   xdim=10,ydim=10
+      fsom <- FlowSOM::SOM(as.matrix(dat[sample(.N,subsample.val), lapply(.SD, function(x) (x - mean(x))/stats::sd(x)),.SDcols=dims]),
+                           xdim=10,ydim=10
       )
     }else{
-      FlowSOM::SOM(as.matrix(dat[sample(.N,subsample.val),..dims]),
-                   xdim=10,ydim=10
+      fsom <- FlowSOM::SOM(as.matrix(dat[sample(.N,subsample.val),dims,with=F]),
+                           xdim=10,ydim=10
       )
     }
   }else{
     set.seed(1337)
     if(scale.dims){
-      FlowSOM::SOM(as.matrix(dat[, lapply(.SD, function(x) (x - mean(x))/sd(x)),.SDcols=dims]),
-                   xdim=10,ydim=10
+      fsom <- FlowSOM::SOM(as.matrix(dat[, lapply(.SD, function(x) (x - mean(x))/stats::sd(x)),.SDcols=dims]),
+                           xdim=10,ydim=10
       )
     }else{
-      FlowSOM::SOM(as.matrix(dat[,..dims]),
-                   xdim=10,ydim=10
+      fsom <- FlowSOM::SOM(as.matrix(dat[,dims,with=F]),
+                           xdim=10,ydim=10
       )
     }
   }
+  if(return.codes){
+    return(fsom$codes)
+  }else{
+    return(fsom)
+  }
 }
 
-generate_clusters <- function(codes=fsom$codes,k=20){
+generate_clusters <- function(codes,k=20){
   FlowSOM::metaClustering_consensus(codes,k)
 }
 
-generate_map_nodes <- function(codes=fsom$codes,dat,scale.dims=T){
-  dims.codes <- colnames(fsom$codes)
+generate_map_nodes <- function(codes,dat,scale.dims=T){
+  dims.codes <- colnames(codes)
   if(scale.dims)
-    FlowSOM:::MapDataToCodes(codes,as.matrix(dat[, lapply(.SD, function(x) (x - mean(x))/sd(x)),.SDcols=dims.codes]))[,1]
+    FlowSOM:::MapDataToCodes(codes,as.matrix(dat[, lapply(.SD, function(x) (x - mean(x))/stats::sd(x)),.SDcols=dims.codes]))[,1]
 }
+
