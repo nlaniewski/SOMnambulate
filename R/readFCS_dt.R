5edf559b5081cf3de257251b704e5a063af022ed
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2022-12-16 15:52:28 -0500

    reworked readFCS_dt functions for mass cyto files

--- R/readFCS_dt.R
+++ R/readFCS_dt.R
@@ -1,4 +1,3 @@
-
 readFCS_dt<-function(fcs.file.path,use.alias=T,use.alias.split=T,
                      asinh.transform=F,cofactor.default=1000,cofactor.mod=NULL,
                      drop.events=T,sensible.reorder=T){
@@ -8,23 +7,31 @@ readFCS_dt<-function(fcs.file.path,use.alias=T,use.alias.split=T,
   if(use.alias==F){
     fcs.tmp <- flowCore::read.FCS(fcs.file.path,transformation = F,truncate_max_range = F)
   }else{
-    fcs.tmp <- flowCore::read.FCS(fcs.file.path,transformation = F,truncate_max_range = F,channel_alias = channels.df[,c('channels','alias')])
+    fcs.tmp <- flowCore::read.FCS(fcs.file.path,transformation = F,truncate_max_range = F,
+                                  column.pattern = paste0(channels.df$channels,collapse = "|")
+    )
   }
-  fcs.tmp <- trim.scatter(fcs.tmp)
-  fcs.tmp <- flowCore::compensate(fcs.tmp,fcs.tmp@description$SPILL)
+  cyto.type<-ifelse(any(grepl("laser",names(fcs.tmp@description),ignore.case = T)),"flow","mass")
+  if(cyto.type=="flow"){
+    fcs.tmp <- trim.scatter(fcs.tmp)
+    fcs.tmp <- flowCore::compensate(fcs.tmp,fcs.tmp@description$SPILL)
+  }
   ##
   dat <- data.table::as.data.table(fcs.tmp@exprs)
-  if(use.alias==T){
-    if(use.alias.split==T){
-      data.table::setnames(dat,channels.df$alias.split)
-    }else if(use.alias.split==F){
-      data.table::setnames(dat,channels.df$alias)
+  ##
+  if(all(names(dat) == channels.df$channels)){
+    if(use.alias==T){
+      if(use.alias.split==T){
+        data.table::setnames(dat,channels.df$alias.split)
+      }else if(use.alias.split==F){
+        data.table::setnames(dat,channels.df$alias)
+      }
     }
   }
   ##
   if(asinh.transform){
-    asinh.cofactors<- stats::setNames(rep(cofactor.default,length(grep("SC|Time",names(dat),invert = T))),
-                                      nm=grep("SC|Time",names(dat),value = T,invert = T)
+    asinh.cofactors<- stats::setNames(rep(cofactor.default,length(grep("SC|Time|Event_length",names(dat),invert = T))),
+                                      nm=grep("SC|Time|Event_length",names(dat),value = T,invert = T)
     )
     if(!is.null(cofactor.mod)){
       if(length(which(names(asinh.cofactors) %in% names(cofactor.mod)))==length(cofactor.mod)){
@@ -59,10 +66,30 @@ generate.channels.frame<-function(fcs.file.path,split.type.position=NULL){
   if(is.null(split.type.position)){
     split.type.position<-split.type.position.agnostic(channels$S)
   }
-  channels.df<-data.frame(channels=channels$N,alias=NA)
+  channels.df<-data.frame(channels=channels$N,alias=NA,alias.split=NA)
   channels.df$alias[sub("N","S",rownames(channels.df)) %in% names(channels$S)] <- channels$S
-  channels.df$alias[is.na(channels.df$alias)] <- channels.df$channels[is.na(channels.df$alias)]
-  channels.df$alias.split<-sapply(strsplit(channels.df$alias,split.type.position$type),'[',split.type.position$position)
+  ##
+  channels.retain <- c("Time","Event_length")
+  channels.df$alias[channels.df$channels %in% channels.retain] <- channels.df$channels[channels.df$channels %in% channels.retain]
+  channels.dismiss <- c("back","bead")#"background","bead"
+  channels.dismiss.index <- grep(paste0(channels.dismiss,collapse = "|"),channels.df$alias,ignore.case = T)
+  channels.df$alias[channels.dismiss.index] <- NA
+  channels.df<-channels.df[-which(is.na(channels.df$alias)),]
+  ##
+  split.these <- which(!channels.df$alias %in% channels.retain)
+  channels.df$alias.split[split.these]<-sapply(strsplit(channels.df$alias[split.these],split.type.position$type),
+                                               '[',
+                                               split.type.position$position
+  )
+  ##
+  non.unique<-which(channels.df$alias.split %in% names(which(table(channels.df$alias.split)>1)))
+  now.unique<-sapply(channels.df$alias[non.unique],function(i){
+    i<-strsplit(i,split.type.position$type)[[1]]
+    i<-paste(i[[split.type.position$position]],i[[split.type.position$position-1]],sep='_')
+  })
+  channels.df$alias.split[non.unique] <- now.unique
+  ##
+  channels.df$alias.split[is.na(channels.df$alias.split)]<-channels.df$alias[is.na(channels.df$alias.split)]
   ##
   return(channels.df)
 }
@@ -104,31 +131,33 @@ drop_events_median_based<-function(dat,median.cut=0.1){
   if(!data.table::is.data.table(dat)){
     stop("Need data.table (class)")
   }
-  fluors<-grep('SC|Time',colnames(dat),invert = T,value = T,ignore.case = T)
+  transform.these<-grep('SC|Time|Event_length',names(dat),invert = T,value = T,ignore.case = T)
   unique(unlist(dat[, lapply(.SD,function(x){
     b<-seq(min(x),max(x),length.out=301)
     h<-graphics::hist(x,breaks=b,plot=F)
     cut<-stats::median(h$counts[h$counts>0])*median.cut
     cut.index<-which((h$counts>cut))
     list(c(which(x<h$breaks[min(cut.index)]),which(x>h$breaks[max(cut.index)])))
-  }), .SDcols=fluors]))
+  }), .SDcols=transform.these]))
 }
 
 drop_events_count_based<-function(dat){
   if(!data.table::is.data.table(dat)){
     stop("Need data.table (class)")
   }
-  fluors<-grep('SC|Time',colnames(dat),invert = T,value = T,ignore.case = T)
+  transform.these<-grep('SC|Time|Event_length',names(dat),invert = T,value = T,ignore.case = T)
   unique(unlist(dat[, lapply(.SD,function(x){
     b<-seq(min(x),max(x),length.out=301)
     h<-graphics::hist(x,breaks=b,plot=F)
     cut.index<-which((h$counts<=3))
-    cut.max.bin<-min(which(sign(h$breaks[cut.index])==1))
-    cut.min.bin<-cut.max.bin-1
-    cut.min<-h$breaks[cut.index][cut.min.bin]
-    cut.max<-h$breaks[cut.index][cut.max.bin]
-    list(c(which(x<cut.min),which(x>cut.max)))
-  }), .SDcols=fluors]))
+    if(length(cut.index)!=0){
+      cut.max.bin<-min(which(sign(h$breaks[cut.index])==1))
+      cut.min.bin<-cut.max.bin-1
+      cut.min<-h$breaks[cut.index][cut.min.bin]
+      cut.max<-h$breaks[cut.index][cut.max.bin]
+      list(c(which(x<cut.min),which(x>cut.max)))
+    }
+  }), .SDcols=transform.these]))
 }
 
 cols.sensible.reorder<-function(cols){
