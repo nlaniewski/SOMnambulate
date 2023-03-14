a7cff0bb5387a6facc7fd42c14c1178e2f9e2010
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2023-03-14 16:05:04 -0400

    updated cluster and node counts

--- R/cluster_node_counts.R
+++ R/cluster_node_counts.R
@@ -1,41 +1,49 @@
-cluster_counts_long <- function(dat,sample.split.cols=c('subject','visit','condition','batch','batch.date')){
-  if(!all(c('sample','cluster') %in% names(dat))){
-    stop("Need 'sample' and 'cluster' columns...")
+cluster_counts_long <- function(dat,cluster.col='cluster',sample.col='sample.id',
+                                sample.split.cols=c('subject','visit','condition','batch','batch.date')){
+  if(!cluster.col %in% names(dat)){
+    stop(paste("Specified cluster column name not found:",cluster.col))
+  }
+  if(!sample.col %in% names(dat)){
+    stop(paste("Specified sample column name not found:",sample.col))
   }
   #commented lines fail 'devtools::check()' due to 'no visible binding for global variable...'
   #data.table::setkey(dat, sample, cluster)
-  data.table::setkeyv(dat, c('sample', 'cluster'))
+  data.table::setkeyv(dat, c(sample.col, cluster.col))
   #dat.N.cluster<-dat[data.table::CJ(sample, cluster, unique = TRUE), .N, by = .EACHI]
-  dat.N.cluster<-dat[data.table::CJ(get('sample'), get('cluster'), unique = TRUE), .N, by = .EACHI]
+  dat.N.cluster<-dat[data.table::CJ(get(sample.col), get(cluster.col), unique = TRUE), .N, by = .EACHI]
   #dat.N.cluster[, prop := N/sum(N) * 100, by = sample]
-  dat.N.cluster[, 'prop' := get('N')/sum(get('N')) * 100, by = sample]
+  dat.N.cluster[, 'prop' := get('N')/sum(get('N')) * 100, by = sample.col]
   #dat.N.cluster[, per1million := N*(1E6/sum(N)), by = sample]
-  dat.N.cluster[, 'per1million' := get('N')*(1E6/sum(get('N'))), by = sample]
+  dat.N.cluster[, 'per1million' := get('N')*(1E6/sum(get('N'))), by = sample.col]
   ##
-  if(!all(sapply(strsplit(dat.N.cluster$sample,"_"),length)==length(sample.split.cols))){
-    stop("Splitting 'sample' string by '_' results in a length != to length of 'sample.split.cols'")
+  if(!all(sapply(strsplit(dat.N.cluster[[sample.col]],"_"),length)==length(sample.split.cols))){
+    stop("Splitting 'sample name' string by '_' results in a length != to length of 'sample.split.cols'")
   }
-  dat.N.cluster[,(sample.split.cols) := data.table::tstrsplit(sample,"_")]
+  dat.N.cluster[,(sample.split.cols) := data.table::tstrsplit(get(sample.col),"_")]
   return(dat.N.cluster)
 }
 ##
-node_counts_long <- function(dat,sample.split.cols=c('subject','visit','condition','batch','batch.date')){
-  if(!all(c('sample','node') %in% names(dat))){
-    stop("Need 'sample' and 'node' columns...")
+node_counts_long <- function(dat,node.col='node',sample.col='sample.id',
+                             sample.split.cols=c('subject','visit','condition','batch','batch.date')){
+  if(!node.col %in% names(dat)){
+    stop(paste("Specified node column name not found:",node.col))
+  }
+  if(!sample.col %in% names(dat)){
+    stop(paste("Specified sample column name not found:",sample.col))
   }
   #commented lines fail 'devtools::check()' due to 'no visible binding for global variable...'
   #data.table::setkey(dat, sample, node)
-  data.table::setkeyv(dat, c('sample', 'node'))
+  data.table::setkeyv(dat, c(sample.col, node.col))
   #dat.N.node<-dat[data.table::CJ(sample, node, unique = TRUE), .N, by = .EACHI]
-  dat.N.node<-dat[data.table::CJ(get('sample'), get('node'), unique = TRUE), .N, by = .EACHI]
+  dat.N.node<-dat[data.table::CJ(get(sample.col), get(node.col), unique = TRUE), .N, by = .EACHI]
   #dat.N.node[, prop := N/sum(N) * 100, by = sample]
-  dat.N.node[, 'prop' := get('N')/sum(get('N')) * 100, by = sample]
+  dat.N.node[, 'prop' := get('N')/sum(get('N')) * 100, by = sample.col]
   #dat.N.node[, per1million := N*(1E6/sum(N)), by = sample]
-  dat.N.node[, 'per1million' := get('N')*(1E6/sum(get('N'))), by = sample]
+  dat.N.node[, 'per1million' := get('N')*(1E6/sum(get('N'))), by = sample.col]
   ##
-  if(!all(sapply(strsplit(dat.N.node$sample,"_"),length)==length(sample.split.cols))){
-    stop("Splitting 'sample' string by '_' results in a length != to length of 'sample.split.cols'")
+  if(!all(sapply(strsplit(dat.N.node[[sample.col]],"_"),length)==length(sample.split.cols))){
+    stop("Splitting 'sample name' string by '_' results in a length != to length of 'sample.split.cols'")
   }
-  dat.N.node[,(sample.split.cols) := data.table::tstrsplit(sample,"_")]
+  dat.N.node[,(sample.split.cols) := data.table::tstrsplit(get(sample.col),"_")]
   return(dat.N.node)
 }
