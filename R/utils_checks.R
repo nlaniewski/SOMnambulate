f28804ff4acbc4b95112dc75f7da2df40d050ed5
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2023-03-15 12:32:27 -0400

    cluster column check

--- R/utils_checks.R
+++ R/utils_checks.R
@@ -4,3 +4,18 @@ sample.id.check<-function(dt.names){
                "check 'names(dat)' and consider renaming the identifier column with 'data.table::setnames()'",sep="\n"))
   }
 }
+##
+cluster.col.check<-function(dt.names){
+  if(!'cluster' %in% dt.names){
+    if(any(grepl('cluster',dt.names,ignore.case = T))){
+      if(length(which(grepl('cluster',dt.names,ignore.case = T)))==1){
+        cluster.col<-grep('cluster',dt.names,ignore.case = T,value=T)
+      }else{
+        c.names<-paste0(grep('cluster',dt.names,ignore.case = T,value=T),collapse = " ; ")
+        stop(paste("More than one cluster column found:",c.names))
+      }
+    }
+  }
+  return(cluster.col)
+}
+##
