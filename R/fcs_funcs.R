df19f40463ab861d9addda04f3db780e6b81a9dc
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2023-09-11 11:04:35 -0400

    fcs.funcs.R added

---
+++ R/fcs_funcs.R
@@ -0,0 +1,31 @@
+#' Get parameters ('$P') from .fcs file headers
+#'
+#' @param fcs.file.paths Character string; path(s) usually returned from \code{list.files(...,full.names=T,pattern=".fcs")}.
+#' @param return.dt Logical. By default, \code{FALSE}; if \code{TRUE}, will return a data.table of parameters per .fcs file
+#'
+#' @return a list of parameters per .fcs file; if \code{return.dt=T}, a data.table of parameters
+#' @export
+#'
+#'
+get.fcs.parameters<-function(fcs.file.paths,return.dt=F){
+  fcs.parameters.list<-sapply(flowCore::read.FCSheader(fcs.file.paths),function(h){
+    p.max<-as.numeric(h['$PAR'])
+    parm.letters<-unique(sub("\\$P[0-9]+","",grep('\\$P[0-9]',names(h),value = T)))
+    p.list<-sapply(parm.letters,function(p){
+      pars<-h[grep(paste0("\\$P[0-9]+",p),names(h))]
+    },simplify = F)
+    for(i in parm.letters){
+      fill<-setdiff(seq(p.max),as.numeric(stringr::str_extract(names(p.list[[i]]),"[0-9]+")))
+      fill.names<-setNames(nm=paste0("$P",fill,i),rep(NA,length(fill)))
+      p.list[[i]]<-c(p.list[[i]],fill.names)
+    }
+    p.list<-lapply(p.list,function(p) p<-p[stringr::str_order(names(p),numeric = T)])
+    return(p.list)
+  },simplify = F)
+  if(return.dt){
+    fcs.parameters.list<-sapply(fcs.parameters.list,function(p) data.table::setDT(p),simplify = F)
+  }else{
+    return(fcs.parameters.list)
+  }
+}
+##
