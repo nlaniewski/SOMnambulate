298a2c614a0c6ae78e2d2908d8f0fe7f2f31b0bb
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2023-10-16 12:20:58 -0400

    return unique data.tables from get.fcs.keywords.metadata

--- R/fcs_funcs.R
+++ R/fcs_funcs.R
@@ -33,12 +33,13 @@ get.fcs.parameters<-function(fcs.file.paths,return.dt=F){
 #' @param fcs.file.paths Character string; path(s) usually returned from \code{list.files(...,full.names=T,pattern=".fcs")}.
 #' @param return.dt Logical. By default, \code{FALSE}; if \code{TRUE}, will return a data.table of keyword/values per .fcs file
 #' @param pattern Pattern used to split keyword character strings; specifically for use with barcoded/pooled .fcs files that contain 'collapsed' metadata.
+#' @param pattern.unique Logical. By default, \code{TRUE}; returns unique keyword data.tables.
 #'
 #' @return a list of non-parameter ('$P') keywords per .fcs file; if \code{return.dt=T}, a data.table of keywords/values
 #' @export
 #'
 #'
-get.fcs.keywords.metadata <- function(fcs.file.paths,return.dt=F,pattern=NULL){
+get.fcs.keywords.metadata <- function(fcs.file.paths,return.dt=F,pattern=NULL,pattern.unique=T){
   fcs.keywords.list <- sapply(flowCore::read.FCSheader(fcs.file.paths),function(h){
     kw<-grep(paste0("\\$",c('B','D','E','M','N','P'),collapse = "|"),names(h),value = T,invert = T)
     return(as.list(h[kw]))
@@ -51,6 +52,15 @@ get.fcs.keywords.metadata <- function(fcs.file.paths,return.dt=F,pattern=NULL){
         data.table::as.data.table(sapply(kws[which(stringr::str_count(kws,pattern)==s)],strsplit,pattern))
       })
     })
+    if(pattern.unique){
+      fcs.keywords.dt.split<-sapply(seq(max(sapply(fcs.keywords.dt.split,length))),function(n){
+        fcs.keywords.dt.split<-sapply(fcs.keywords.dt.split,'[',n)
+        if(length(unique(fcs.keywords.dt.split))==1) fcs.keywords.dt.split<-fcs.keywords.dt.split[[1]]
+        return(fcs.keywords.dt.split)
+      })
+    }else{
+      return(fcs.keywords.dt.split)
+    }
   }else{
     return(fcs.keywords.list)
   }
