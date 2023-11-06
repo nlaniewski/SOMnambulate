1c215cbde056e7763599eba73bcf6fd97007c7fe
nlaniewski <nathan_laniewski@urmc.rochester.edu>
2023-11-06 16:17:03 -0500

    detect existing clusters

--- R/fsom_funcs.R
+++ R/fsom_funcs.R
@@ -6,22 +6,39 @@
 #' @param cluster.col Optional character string; if defined, will rename the otherwise generically named 'cluster' column in the median results
 #'
 #' @details
-#' \code{fsom.codes.to.clusters} uses \code{fsom$codes} to first generate metaclusters, then calculates per-cluster median expression values for use primarily in heatmaps/visualizations.
+#' \code{fsom.codes.to.clusters} uses \code{fsom$codes} to first generate metaclusters, then calculates per-cluster median expression values for use primarily in heatmaps/visualizations. If factored clusters already exist (\code{fsom$cluster$fac}), medians will be updated -- useful following \code{fsom.merge.codes}.
 #'
 #' @return FlowSOM object appended with list-structured results
 #' @export
 #'
 #'
 fsom.codes.to.clusters<-function(fsom,k=20,seed=1337,cluster.col=NULL){
+  ##
+  existing.clusters<-!is.null(fsom$cluster$fac)
+  ##
   dims<-colnames(fsom$codes)
-  cluster.ls<-list(cluster=list(fac=NULL,dat=list(medians=NULL,medians.scaled=NULL,medians.01=NULL)))
-  cluster.ls[['cluster']][['fac']]<-factor(FlowSOM::metaClustering_consensus(fsom$codes, k = k, seed=seed))
-  cluster.ls[['cluster']][['dat']][['medians']]<-data.table::data.table(fsom$codes,cluster=cluster.ls[['cluster']][['fac']])[
+  cluster.ls<-list(cluster=list(fac=if(existing.clusters){fsom$cluster$fac}else{NULL},
+                                dat=list(medians=NULL,
+                                         medians.scaled=NULL,
+                                         medians.01=NULL
+                                ))
+  )
+  ##
+  if(existing.clusters){
+    message("Existing clusters; regenerating medians...")
+    cluster.ls$cluster$fac<-fsom$cluster$fac
+  }else{
+    message(paste("Generating",k,"clusters using FlowSOM::metaClustering_consensus"))
+    cluster.ls$cluster$fac <- factor(FlowSOM::metaClustering_consensus(fsom$codes,
+                                                                       k = k, seed = seed))
+  }
+  ##
+  cluster.ls$cluster$dat$medians<-data.table::data.table(fsom$codes,cluster=cluster.ls$cluster$fac)[
     ,lapply(.SD,stats::median),keyby = cluster]
-  if(!is.null(cluster.col)) data.table::setnames(cluster.ls[['cluster']][['dat']][['medians']],'cluster',cluster.col)
-  cluster.ls[['cluster']][['dat']][['medians.scaled']]<-data.table::copy(cluster.ls[['cluster']][['dat']][['medians']])[
+  if(!is.null(cluster.col)) data.table::setnames(cluster.ls$cluster$dat$medians,'cluster',cluster.col)
+  cluster.ls$cluster$dat$medians.scaled<-data.table::copy(cluster.ls$cluster$dat$medians)[
     ,(dims) := lapply(.SD, function(x) (x - mean(x))/stats::sd(x)),.SDcols = dims]
-  cluster.ls[['cluster']][['dat']][['medians.01']]<-data.table::copy(cluster.ls[['cluster']][['dat']][['medians']])[
+  cluster.ls$cluster$dat$medians.01<-data.table::copy(cluster.ls$cluster$dat$medians)[
     ,(dims) := lapply(.SD,function(x){scales::rescale(x, from=c(0, max(x)))}),.SDcols = dims]
   return(append(fsom,cluster.ls))
 }
